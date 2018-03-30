9992739fc6072e1117780debf2162af270c7b6f4
Salinger MS <minsiua@gmail.com>
2018-03-31 02:43:55 +0900

    webpack css loading

---
+++ src/image_viewer.js
@@ -0,0 +1,6 @@
+import '../styles/image_viewer.css';
+
+const image = document.createElement('img');
+image.src = 'http://placehold.it/500x200&text=OriginalEXE';
+
+document.body.appendChild(image);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //import or require sum.js needed
-const sum = require('./sum');
+import sum from './sum';
+import './image_viewer';
 
 const total = sum(5,10);
 console.log(total);
