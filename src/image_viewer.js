66e2a04b33a577caf63195c488b83810f5b978ef
Salinger MS <minsiua@gmail.com>
2018-03-31 07:50:11 +0900

    simple code spliting

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,14 +1,16 @@
 import '../styles/image_viewer.css';
-import img1 from '../assets/1.jpg';
+// import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
 // import img3 from '../assets/3.jpg';
 
-const image1 = document.createElement('img');
-image1.src = img1;
+// const image1 = document.createElement('img');
+// image1.src = img1;
 
-document.body.appendChild(image1);
+// document.body.appendChild(image1);
 
-const image2 = document.createElement('img');
-image2.src = img2;
-
-document.body.appendChild(image2);
+export default () => {
+    const image2 = document.createElement('img');
+    image2.src = img2;
+    
+    document.body.appendChild(image2);
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 //import or require sum.js needed
-import sum from './sum';
-import './image_viewer';
+// import sum from './sum';
+// import './image_viewer';
 
-const total = sum(5,10);
-console.log(total);
+// const total = sum(5,10);
+// console.log(total);
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+    System.import('./image_viewer').then(module => {
+        module.default();
+        console.log(module);
+    });
+};
+
+document.body.appendChild(button);
